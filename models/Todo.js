b6abaccf4a5a2148a697e0f3e59af458b23d9e08
kooose38 <75005025+kooose38@users.noreply.github.com>
2020-12-24 20:42:21 +0900

    initSetting / dataCreate

---
+++ models/Todo.js
@@ -0,0 +1,28 @@
+const todos = []
+
+let num = 1;
+
+class Todo {
+   constructor(title, body) {
+      this.id = num++;
+      this.ttile = title;
+      this.body = body;
+      this.createdAt = new Date();
+      this.updatedAt = new Date();
+   }
+}
+
+for (let i = 0; i < 5; i++) {
+   const index = i + 1;
+   const title = "本文" + index;
+   const body = "内容" + index;
+   const todo = new Todo(title, body);
+   todos.push(todo)
+}
+
+
+module.exports = {
+   getTodos: () => {
+      return todos
+   },
+}
